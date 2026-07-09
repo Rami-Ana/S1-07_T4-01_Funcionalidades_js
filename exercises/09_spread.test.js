909cb26e5ce8f27be559315e7e31d04836186fc1
Ana Rami <ana.ramimoreno@gmail.com>
2026-07-09 20:21:51 +0200

    feat: complete exercise Spread

--- exercises/09_spread.test.js
+++ exercises/09_spread.test.js
@@ -1,4 +1,7 @@
 test('09_spread-1: should be able to call a function and spread the arguments', () => {
+//   spread:
+//   numeros        → [1,2,3] (un solo elemento)
+//   numeros     → 1,2,3   (elementos separados)
   // Utilitza l'operador spread per passar arguments a una funció
   const args = ['a', 'b', 'c']
   let calls = 0
@@ -9,7 +12,10 @@ test('09_spread-1: should be able to call a function and spread the arguments', 
     expect(c).toBe('c')
     calls++
   }
-  
+  //myFunction(['a', 'b', 'c']) // sin spread
+  myFunction(...args) //los ... son spreads: enviando/expandiendo parámetros.
+  // ===spread para pasar argumentos a una función)
+//myFunction('a', 'b', 'c') con spread
   expect(calls).toBe(1)
 
 })
@@ -17,6 +23,8 @@ test('09_spread-1: should be able to call a function and spread the arguments', 
 test('09_spread-2: should be easier to concatenate arrays', () => {
   // Crea un segon array amb els valors `[4, 5, 6]` i utilitza l'operador spread (`...`) per concatenar-lo amb `array1`.
   const array1 = [1, 2, 3]
+  const array2 = [4, 5, 6]
+  const result = [...array1, ...array2]  //spread para crear arrays
   
   
   expect(result).toEqual([1, 2, 3, 4, 5, 6])
@@ -25,10 +33,14 @@ test('09_spread-2: should be easier to concatenate arrays', () => {
 test('09_spread-3: should be able to merge properties from objects', () => {
   // Crea un nou objecte `result` utilitzant l'operador spread (`...`) per copiar les propietats de `obj1` i afegir la propietat que es mostra al test.
   const obj1 = {
-    foo: 'bar',
+    foo: 'bar', //propiedades
     baz: 'foobar',
   }
 
+const result = { //creamos un obj usando spread para copiar propiedad de obj1
+  ...obj1, //Expande las propiedades del objeto:
+  eggs: 'spam' //y añadir otra
+}
 
   expect(result).toEqual({
     foo: 'bar',
