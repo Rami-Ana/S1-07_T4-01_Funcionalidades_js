5228a0df93b46cc4df971c656f894567e4a6ecf9
Ana Rami <ana.ramimoreno@gmail.com>
2026-07-10 11:38:01 +0200

    feat: complete exercise Class

--- exercises/10_class.test.js
+++ exercises/10_class.test.js
@@ -2,6 +2,11 @@ test('10_class-1: has a constructor for initialization', () => {
   // Crea una classe Animal
   // Afegeix un constructor que prengui un paràmetre, el nom.
   // Assigna this.name al nom passat com a paràmetre
+  class Animal {
+     constructor(name) {
+       this.name = name; //guardar ese valor en la propiedad this.name, asign parametro.
+  }
+}
 
   const animal = new Animal()
   const dog = new Animal('Dog')
@@ -13,9 +18,15 @@ test('10_class-1: has a constructor for initialization', () => {
 test('10_class-2: constructor can have default param values', () => {
   // Crea una classe Animal amb un constructor
   // Fes que la classe tingui un valor per defecte (utilitzant paràmetres per defecte) per al nom: 'Honey Badger'
-
-  const animal = new Animal()
+  class Animal {
+     constructor(name = 'Honey Badger') { 
+       this.name = name;
+  }
+}
+  const animal = new Animal() //No pasamos ningún argumento
   const dog = new Animal('Dog')
+//   name (en constructor(name)) es el parámetro.
+//  'Dog' (en new Animal('Dog')) es el argumento o valor que pasas al parámetro.
 
   expect(animal.name).toBe('Honey Badger')
   expect(dog.name).toBe('Dog')
@@ -23,17 +34,30 @@ test('10_class-2: constructor can have default param values', () => {
 
 test('10_class-3: can have instance methods', () => {
   // Crea una classe Animal, passa el nom al constructor i afegeix una funció sayName a la definició de la classe
+  class Animal{ constructor(name = 'Honey Badger') {// CONTENEDOR inicialización
+    this.name = name;
+  }
+  sayName(){// METODO INSTANCIA: comportamiento 
+        return `My name is: ${this.name}`;
+  }
+}
+  const animal = new Animal() //creamos un objeto con molde de class Animal
 
-  const animal = new Animal()
-
-  expect(animal.sayName).toBeDefined()
-  expect(Animal.sayName).toBeUndefined()
+  expect(animal.sayName).toBeDefined() // Existe: es una instancia creada a partir del molde Animal
+  expect(Animal.sayName).toBeUndefined() // No existe: es solo un molde class Animal, no objeto
   expect(animal.sayName()).toBe('My name is: Honey Badger')
 })
 
 test('10_class-4: can have static methods', () => {
   // Crea una classe Animal, passa el nom al constructor,
   // i afegeix un mètode estàtic create que prengui un nom i retorni una instància
+
+  class Animal{ constructor (name){
+    this.name = name;
+  }
+  static create(name){ //método estático
+  return new Animal(name); //Que metodo estatico devuuelva una instancia, un objeto creado
+}}
 
   const animal = new Animal()
   const dog = Animal.create('Dog')
@@ -49,6 +73,18 @@ test('10_class-5: can extend another class', () => {
   // Crea una classe Animal
   // Crea una classe Dog que extengui Animal
   // Afegeix un mètode `sayName` a `Dog` que retorni el nom de la instància.
+  class Animal { constructor(name){
+    this.name = name;
+  }}
+
+  class Dog extends Animal {
+  constructor(name) {
+  super(name); //herencia
+  }
+  sayName(){
+    return `My name is: ${this.name}`
+  }
+}
 
   const dog = new Dog('Fido')
 
@@ -65,6 +101,11 @@ test('10_class-6: can use property setters and getters', () => {
   // Crea una classe Animal (no passis el nom al constructor)
   // Afegeix un setter per a la propietat name
   // Afegeix un getter per a la propietat name
+  class Animal{
+    set name(value) {this._name = value;} //value = lo que entra. | _name = lo que se guarda.
+    get name() {return `${this._name} type of animal`;}
+
+  }
 
   const animal = new Animal()
   animal.name = 'Dog'
